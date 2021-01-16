9d1526b99d06772f720f8455670eca3b13473f83
ChalkHuman <isacisboss718@gmail.com>
2021-01-15 18:15:00 -0600

    Download audio

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
+from pytube import YouTube
 import re
 import requests
 import urllib.request
 
-artist = input('Artist name: ')
-song_name = input('Song name: ')
+
+artist = input('Artist Name: ')
+song_name = input('Song Name: ')
 
 def search_youtube():
 	search_query = artist.replace(' ', '+') + '+' + song_name.replace(' ', '+')
@@ -13,4 +15,12 @@ def search_youtube():
 	video = 'https://youtube.com/watch?v=' + video_ids[0]
 	return video
 
-print(search_youtube())
+
+def download_video():
+	print('starting audio download...')
+	yt = YouTube(search_youtube())
+	t = yt.streams.filter(only_audio=True)
+	t[0].download('./audio')
+	print('done downloading')
+
+download_video()
